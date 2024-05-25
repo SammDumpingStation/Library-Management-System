9444abeb00b122f66ec1ad6b22825cbe79ae09c5
Samm Caagbay <sammcaag@gmail.com>
2024-05-25 10:53:40 +0800

    🚀 Setting up the general css

---
+++ landing.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/public/css/general.css">
+  <link rel="stylesheet" href="/public/css/landing.css">
+  <title>LIBRA</title>
+</head>
+<body>
+  <header>
+    <nav class="landing-nav">
+      <h1>LIBRA</h1>
+      <button>Sign-up</button>
+      <button>Log-in</button>
+    </nav>
+  </header>
+</body>
+</html>
